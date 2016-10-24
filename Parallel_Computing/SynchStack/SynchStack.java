c0ab9c866c94d4c40f8ac1296c17e516621a7fd0
andirs <andirs@gmail.com>
2016-10-24 13:22:19 -0700

    Refined comments

--- Parallel_Computing/SynchStack/SynchStack.java
+++ Parallel_Computing/SynchStack/SynchStack.java
@@ -30,9 +30,10 @@ public class SynchStack
      */
     public void push(Object o)
     {
+        stackList.add(0, o);
+        
         // Add 1 to semaphore counter in order 
         // to signal it's possible to get an Object
-        stackList.add(0, o);
         sem.v();
     }
     
@@ -43,7 +44,11 @@ public class SynchStack
      */
     public Object pop()
     {
+        // Subtract 1 of semaphore counter in order to see
+        // if there are enough elements in Stack.
+        // If not, the thread will wait. 
         sem.p();
+
         Object returnItem = stackList.get(0);
         stackList.remove(0);
         return returnItem;
